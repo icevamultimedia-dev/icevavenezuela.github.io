8ef0741d741dabbdf2b38f3b324246a374bc2073
icevavenezuela <icevavenezuela@gmail.com>
2026-03-16 21:57:12 -0400

    Subida limpia desde cero

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true, // set to false when using @vercel/analytics@1.4.0
+    },
+  }),
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
