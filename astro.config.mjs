abb84b6b4dca59629a6cebc04dedf06a03320352
icevavenezuela <icevavenezuela@gmail.com>
2026-03-26 20:12:38 -0400

    Configuración SEO, Robots y Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,21 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   adapter: vercel({
     webAnalytics: {
       enabled: true, // set to false when using @vercel/analytics@1.4.0
     },
   }),
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
